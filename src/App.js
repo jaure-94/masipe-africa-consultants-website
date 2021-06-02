c750ca9b967ac7334b838f6a4aa110756ec4df6f
Tawanda Jaure <tawandajaujau@gmail.com>
2021-06-02 21:00:36 +0200

    Create Header & Theme components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import theme from './components/Theme';
+import { ThemeProvider } from '@material-ui/styles';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Header />
+        </ThemeProvider>
+      </Router>
+    );
+  }
+  
 }
 
 export default App;
