13103c4f70b452cdfc6e2f26b73328448ddff29e
Tawanda Jaure <tawandajaujau@gmail.com>
2021-06-03 11:24:45 +0200

    Add color palette to Theme component
    lightBlue = '#009ade'
    midBlue = '#002e86'
    darkBlue = '#11216a'

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,3 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({});
+const lightBlue = '#009ade',
+      midBlue = '#002e86',
+      darkBlue = '#11216a';
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      lightBlue: `${lightBlue}`,
+      midBlue: `${midBlue}`,
+      darkBlue: `${darkBlue}`
+    },
+    primary: {
+      main: `${lightBlue}`
+    },
+    secondary: {
+      main: `${midBlue}`
+    }
+  }
+});
